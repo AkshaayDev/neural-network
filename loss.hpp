5490b2f4f4d20e069c1e3328183ef5920868bb56
Akshaay Kaldev <163286127+AkshaayDev@users.noreply.github.com>
2025-08-26 19:21:17 +0800

    Create loss.hpp

---
+++ loss.hpp
@@ -0,0 +1,24 @@
+#ifndef LOSS_HPP
+#define LOSS_HPP
+
+namespace NNLoss {
+	// Mean squared error
+	// MSE = 1/n * ∑(ŷ_i - y_i)^2
+	double MSE(NNMatrix predicted, NNMatrix real) {
+		double result = 0;
+		predicted.forEach([real, &result](double *val, int i, int j) {
+			result += std::pow(*val - real.data[i][j], 2);
+		});
+		return result / real.rows();
+	}
+	// Derivative of mean squared error
+	// MSE' = 2/n * (ŷ_i - y_i)
+	NNMatrix MSEDerivative(NNMatrix predicted, NNMatrix real) {
+		predicted.forEach([&real](double *val, int i, int j) {
+			*val = 2.0 / real.rows() * (*val - real.data[i][j]);
+		});
+		return predicted;
+	}
+};
+
+#endif
