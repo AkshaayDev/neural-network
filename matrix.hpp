da9ccdc5c3438edec3bcf6f7b71c76dde9e744fd
Akshaay Kaldev <163286127+AkshaayDev@users.noreply.github.com>
2025-08-26 19:19:57 +0800

    Create matrix.hpp

---
+++ matrix.hpp
@@ -0,0 +1,89 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <cmath>
+#include <random>
+
+// Minimal matrix class
+class NNMatrix {
+public:
+	std::vector<std::vector<double>> data;
+
+	// Constructors
+	NNMatrix() {}
+	NNMatrix(std::vector<std::vector<double>> data) : data(data) {}
+
+	// Getters
+	int rows() { return data.size(); }
+	int cols() { return data[0].size(); }
+
+	// Print the matrix
+	static void print(NNMatrix m) {
+		for (int i = 0; i < m.rows(); i++) {
+			for (int j = 0; j < m.cols(); j++) {
+				std::cout << m.data[i][j] << " ";
+			}
+			std::cout << "\n";
+		}
+	}
+	// Resize the number of rows and columns
+	void resize(int rows, int cols) {
+		data.resize(rows);
+		for (int i = 0; i < rows; i++) {
+			data[i].resize(cols);
+		}
+	}
+	// Apply a function to each element of the matrix with its value, row and column
+	void forEach(const std::function<void(double*, int, int)>& func) {
+		for (int i = 0; i < rows(); i++) {
+			for (int j = 0; j < cols(); j++) {
+				func(&data[i][j], i, j);
+			}
+		}
+	}
+	// Dot product of two matrices
+	static NNMatrix dot(NNMatrix a, NNMatrix b) {
+		NNMatrix result;
+		result.resize(a.rows(), b.cols());
+		for (int i = 0; i < a.rows(); i++) {
+			for (int j = 0; j < b.cols(); j++) {
+				for (int k = 0; k < b.rows(); k++) {
+					result.data[i][j] += a.data[i][k] * b.data[k][j];
+				}
+			}
+		}
+		return result;
+	}
+	// Element-wise addition
+	NNMatrix operator+(NNMatrix b) {
+		NNMatrix res = *this;
+		res.forEach([b](double *val, int i, int j) {
+			*val += b.data[i][j];
+		});
+		return res;
+	}
+	// Element-wise multiplication
+	NNMatrix operator*(NNMatrix b) {
+		NNMatrix res = *this;
+		res.forEach([b](double *val, int i, int j) {
+			*val *= b.data[i][j];
+		});
+		return res;
+	}
+	// Transpose the matrix (Switch rows and columns)
+	NNMatrix transpose() {
+		NNMatrix res;
+		res.resize(cols(), rows());
+		for (int i = 0; i < rows(); i++) {
+			for (int j = 0; j < cols(); j++) {
+				res.data[j][i] = data[i][j];
+			}
+		}
+		return res;
+	}
+};
+
+#endif
