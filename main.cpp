02aa53af4b7ba0ab040bb1102d88cba6f1dce139
Akshaay Kaldev <163286127+AkshaayDev@users.noreply.github.com>
2025-08-26 19:19:11 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "matrix.hpp"
+#include "nn.hpp"
+#include "activation.hpp"
+#include "loss.hpp"
+#include "inits.hpp"
+
+int main() {
+	// We will be using a simple 2,2,1 architecture to test the XOR gate
+	NeuralNetwork nn;
+	nn.setLayers({2,2,1});
+	nn.initialisationFn = NNInitialisations::xavierInitialisation;
+	nn.initialise();
+	nn.activationFn = NNActivation::sigmoid;
+	nn.activationFnDerivative = NNActivation::sigmoidDerivative;
+	nn.lossFn = NNLoss::MSE;
+	nn.lossFnDerivative = NNLoss::MSEDerivative;
+	std::vector<std::pair<NNMatrix, NNMatrix>> data = {
+		{ NNMatrix({{0},{0}}), NNMatrix({{0}}) },
+		{ NNMatrix({{0},{1}}), NNMatrix({{1}}) },
+		{ NNMatrix({{1},{0}}), NNMatrix({{1}}) },
+		{ NNMatrix({{1},{1}}), NNMatrix({{0}}) }
+	};
+	nn.batchGradientDescent(0.1, 100000, data);
+	for (std::pair<NNMatrix, NNMatrix> sample : data) {
+		std::cout << sample.first.data[0][0];
+		std::cout << " ^ ";
+		std::cout << sample.first.data[1][0];
+		std::cout << " = ";
+		NNMatrix::print(nn.run(sample.first));
+	}
+}
