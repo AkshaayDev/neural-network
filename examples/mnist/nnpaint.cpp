104825926c65ab1a0f6703edb370c9d0751ec208
AkshaayDev <163286127+AkshaayDev@users.noreply.github.com>
2025-11-10 23:40:41 +0800

    Use raylib instead of SFML for nnpaint

--- examples/mnist/nnpaint.cpp
+++ examples/mnist/nnpaint.cpp
@@ -1,36 +1,27 @@
 /*
-This is a simple 28x28 grid paint app that shows the network's guess of the digit drawn
+This is a simple 28x28 grid paint app made with raylib that shows the network's guess of the digit drawn
 This script reads the network from a file `nn.dat` and uses it to predict the digit
-This script also requires a font file (`arial.ttf` in this case) to display the text
 
 Usage:
-1. Download SFML from the official website: https://www.sfml-dev.org/download/
-2. Follow the instructions to set up an SFML project
-3. Copy and paste this code into the main C++ file
-4. Ensure that a `nn.dat` file exists in the same directory
-5. Add a font file of a type supported by SFML and include its path in `fontPath`
-6. Compile and run
+1. Download or install raylib (Official website: https://www.raylib.com/)
+2. Ensure that a `nn.dat` file exists in the same directory as this file
+3. Compile with raylib and run
 
 Controls:
 Left Mouse: White brush
 Right Mouse: Black brush/Erase
 'C' Key: Clear canvas
 */
-#include <SFML/Graphics.hpp>
 #include "../../neural-network.hpp"
-#include <iostream>
-#include <string>
-#include <fstream>
+#include "raylib.h"
 
-const std::string fontPath = "./arial.ttf";
-const int squareSize = 20; // Display size of a pixel on the grid
+NNMatrix pixels(28 * 28, 1);
+const int pixelScale = 20;
 const int brushSize = 2;
-// Represents a flattened column matrix of the grid
-NNMatrix img(28*28,1);
 
-static void draw(sf::RenderWindow& window, int white) {
-	int x = sf::Mouse::getPosition(window).x / squareSize;
-	int y = sf::Mouse::getPosition(window).y / squareSize;
+static void draw(int delta) {
+	int x = GetMouseX() / pixelScale;
+	int y = GetMouseY() / pixelScale;
 	if (x < 0 || x >= 28 || y < 0 || y >= 28) return;
 
 	for (int i = -brushSize; i < brushSize; i++) {
@@ -40,107 +31,83 @@ static void draw(sf::RenderWindow& window, int white) {
 			if (nx < 0 || nx >= 28 || ny < 0 || ny >= 28) continue;
 
 			// Normalized distance from center
-			double d = std::sqrt(std::pow(i, 2) + std::pow(j, 2)) / brushSize;
+			double d = std::hypot(i, j) / brushSize;
 			double intensity = 0;
 			if (d < 1) {
 				// Apply cos^2(d * pi/2)
 				intensity = std::pow(std::cos(d * 3.1415926536 / 2), 2);
 			}
-			double& pixel = img[28 * ny + nx][0];
+			double& pixel = pixels[28 * ny + nx][0];
 			// Clamp between 0-1
-			pixel = std::max(0.0, std::min(1.0, pixel + (white * intensity)));
+			pixel = std::clamp(pixel + (delta * intensity), 0.0, 1.0);
 		}
 	}
 }
 
-int main() {
-	try {
-		// Load network data from `./nn.dat`
-		NeuralNetwork nn;
-		std::ifstream in("./nn.dat", std::ios::binary);
-		if (in.good()) nn.load(in);
-		else std::cerr << "Could not open network data file\n";
-		in.close();
+int main(void) {
+	// Load network data from `./nn.dat`
+	NeuralNetwork nn;
+	std::ifstream in("./nn.dat", std::ios::binary);
+	if (in.good()) nn.load(in);
+	else std::cerr << "Could not open network data file\n";
+	in.close();
 
-		// Create a window
-		sf::RenderWindow window(sf::VideoMode({ 750,500 }), "NNPaint");
+	// Create the window
+	InitWindow(28 * pixelScale + 200, 28 * pixelScale, "NNPaint");
+	SetTargetFPS(60);
+	SetConfigFlags(FLAG_VSYNC_HINT);
 
-		// Load Arial font
-		// Note: openFromFile() doesn't seem to find the file even if it exists
-		// So, the font file is loaded into a std::vector<char> buffer via std::ifstream
-		// Then, the font is loaded from that buffer memory
-		std::ifstream file(fontPath, std::ios::binary);
-		std::vector<char> buffer((std::istreambuf_iterator<char>(file)), {});
-		sf::Font font;
-		if (!font.openFromMemory(buffer.data(), buffer.size())) {
-			std::cerr << "Failed to load font from memory.\n";
-			return 1;
+	std::vector<Color> pixelColors(28 * 28);
+	Image img = GenImageColor(28, 28, BLACK);
+	Texture2D texture = LoadTextureFromImage(img);
+
+	while (!WindowShouldClose()) {
+		// Left Mouse: White brush
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) draw(1);
+		// Right Mouse: Black brush/Erase
+		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) draw(-1);
+		// 'C' Key: Clear canvas
+		if (IsKeyPressed(KEY_C))
+			for (int i = 0; i < 28 * 28; i++)
+				pixels[i][0] = 0;
+
+		// Fill the color map and update the texture using the pixel matrix
+		for (int i = 0; i < 28*28; i++) {
+			unsigned char intensity = static_cast<unsigned char>(pixels[i][0] * 255);
+			pixelColors[i] = { intensity, intensity, intensity, 255 };
 		}
-		std::cout << "Font loaded successfully!\n";
+		UpdateTexture(texture, pixelColors.data());
 
-		while (window.isOpen()) {
-			while (const std::optional event = window.pollEvent()) {
-				if (event->is<sf::Event::Closed>())
-					window.close();
-			}
-			window.clear();
-			// Left Mouse: White brush
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
-				draw(window, 1);
-			// Right Mouse: Black brush/Erase
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right))
-				draw(window, -1);
-			// 'C' Key: Clear canvas
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::C))
-				for (int i = 0; i < 28 * 28; i++)
-					img[i][0] = 0;
-			// Display canvas
-			for (int i = 0; i < 28; i++) {
-				for (int j = 0; j < 28; j++) {
-					sf::RectangleShape square({ squareSize, squareSize });
-					int intensity = static_cast<int>(img[28 * j + i][0] * 255);
-					square.setFillColor(sf::Color(intensity, intensity, intensity));
-					square.setPosition({ static_cast<float>(squareSize * i), static_cast<float>(squareSize * j) });
-					square.setOutlineColor(sf::Color(127, 127, 127));
-					square.setOutlineThickness(1);
-					window.draw(square);
-				}
-			}
-			// Display prediction
-			NNMatrix res = nn.run(img);
-			for (int i = 0; i < 10; i++) {
-				sf::CircleShape circle;
-				circle.setRadius(20.f);
-				circle.setPosition({ static_cast<float>(580),static_cast<float>(25 + 40 * i) });
-				int intensity = static_cast<int>(res[i][0] * 255);
-				sf::Color opp = (intensity > 128) ? sf::Color::Black : sf::Color::White;
-				circle.setFillColor(sf::Color(intensity, intensity, intensity));
-				circle.setOutlineColor(opp);
-				circle.setOutlineThickness(1);
-				window.draw(circle);
+		BeginDrawing();
+		ClearBackground({ 20, 20, 20, 255 });
 
-				sf::Text label(font);
-				// Note: using a string directly in setString() throws a memory runtime error
-				// So, it is converted into a C string (const char*)
-				label.setString(std::to_string(i).c_str());
-				label.setCharacterSize(20); // in pixels
-				label.setFillColor(opp);
-				label.setPosition({ static_cast<float>(595),static_cast<float>(32 + 40 * i) });
-				window.draw(label);
+		// Draw the grid
+		DrawTextureEx(texture, { 0,0 }, 0, pixelScale, WHITE);
+		DrawText("Left Mouse: White brush", 10, 10, 20, DARKGRAY);
+		DrawText("Right Mouse: Black brush", 10, 30, 20, DARKGRAY);
+		DrawText("\'C\' Key: Clear canvas", 10, 50, 20, DARKGRAY);
 
-				sf::Text confidence(font);
-				std::string val = std::to_string(res[i][0] * 100) + "%";
-				confidence.setString(val.c_str()); // Convert to C string
-				confidence.setCharacterSize(20); // in pixels
-				int halfIntensity = 128 + intensity / 2;
-				confidence.setFillColor(sf::Color(halfIntensity, halfIntensity, halfIntensity));
-				confidence.setPosition({ static_cast<float>(630),static_cast<float>(32 + 40 * i) });
-				window.draw(confidence);
-			}
-			window.display();
+		// Draw the predictions
+		NNMatrix res = nn.run(pixels);
+		for (int i = 0; i < 10; i++) {
+			int x = 28 * pixelScale + 40;
+			int y = 50 + i * 50;
+			unsigned char intensity = res[i][0] * 255;
+			unsigned char whitened = 128 + intensity / 2;
+			Color confidence = { intensity, intensity, intensity, 255 };
+			Color contrast = (intensity > 127) ? BLACK : WHITE;
+			Color visible = { whitened, whitened, whitened, 255 };
+			// Draw a circle with the brightness corresponding to confidence
+			DrawCircle(x, y, 20, confidence);
+			// Draw its number label and outline with a contrasting color
+			DrawCircleLines(x, y, 20, contrast);
+			DrawText(std::to_string(i).c_str(), x - 5, y - 10, 20, contrast);
+			// Draw its confidence percentage with a more visible color
+			std::string percentage = std::to_string(res[i][0] * 100) + "%";
+			DrawText(percentage.c_str(), x + 30, y - 10, 20, visible);
 		}
+		EndDrawing();
 	}
-	catch (sf::Exception& e) {
-		std::cout << "Exception: " << e.what();
-	}
+	CloseWindow();
+	return 0;
 }
